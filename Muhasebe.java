e266dcde68814016c06ff90099d048901faa4253
ceryigit <ceryigit@lely.com>
2024-05-30 15:27:49 +0300

    Ders6 Ödev

--- Muhasebe.java
+++ Muhasebe.java
@@ -1,14 +1,10 @@
 public class Muhasebe {
     public static void main(String[] args) {
-        int totalday = 30;
+        int totalday = 26;
         int maas = 10000;
         int prim = totalday-25;
-        if (totalday > 25 ) {
+        int sonuc = (totalday > 25 ) ? maas+= 1000*prim : maas;
+        System.out.println(sonuc);
 
-            maas+= 1000*prim;
-            System.out.println(maas);
-
-        }
-        else System.out.println(maas);
     }
 }
