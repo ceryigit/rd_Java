6f9be86e713c4e68be58404a1f877416802256a2
ceryigit <ceryigit@lely.com>
2024-07-14 21:49:26 +0300

    Ders15 Ödev

---
+++ Ders15OdevEdited/Singleton.java
@@ -0,0 +1,39 @@
+package Ders15OdevEdited;
+
+public class Singleton {
+    private static Singleton instance;
+    private String ad, soyad;
+    private int yas;
+    private Singleton() {
+        this.ad="Ceylan";
+        this.soyad="Eryiğit";
+        this.yas=28;
+
+    }
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public int getYas() {
+        return yas;
+    }
+
+    public void setYas(int yas) {
+        this.yas = yas;
+    }
+
+    public void displayInfo() {
+        System.out.println("İsim: " + ad +"\n" + "Soyisim: " + soyad +"\n"+ "Yaş: " + yas);
+    }
+}
