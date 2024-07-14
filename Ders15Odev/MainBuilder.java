26d899142ebf4add7d58f1b2e0efbf009f3bbeba
ceryigit <ceryigit@lely.com>
2024-07-14 21:42:52 +0300

    Ders15 Ödev

---
+++ Ders15Odev/MainBuilder.java
@@ -0,0 +1,13 @@
+package Ders15Odev;
+
+public class MainBuilder {
+    public static void main(String[] args) {
+        Sports sports = Sports.SportsBuilder.sport()
+                .aboutFutbol("Euro 2024'de Türkiye çeyrek finalde Hollandaya karşı mağlubiyet almıştır. \n")
+                .aboutVoleybol("VNL'de Kadın A Milli Takımımız çeyrek finalde Polonya'ya karşı mağlubiyet almıştır.\n")
+                .aboutTenis("2024 Wibledon tek erkekler şampiyonu Alcaraz olmuştur.\n")
+                .build();
+        System.out.println(sports.toString());
+    }
+
+}
